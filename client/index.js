9784beff1325b816bc684077f2c5c364f376b997
Dana Tran <dana.l.tran@gmail.com>
2016-09-09 15:47:29 -0700

    added save board button

--- client/index.js
+++ client/index.js
@@ -2,35 +2,24 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Board from './Board';
 import Player from './Player';
-import FormSave from './FormSave';
+
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      username: 'funkmaster',
       board: [
         [1,0,0,0,1,1,1,1],
         [0,0,1,0,0,0,0,0],
         [0,0,0,0,0,0,0,0],
         [0,1,0,1,1,1,1,1]
-      ],
-      otherBoards: {
-        boardOne: [
-          [0,0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0,0],
-          [0,0,0,0,0,0,0,0]
-        ],
-        boardTwo: [
-          [1,1,1,1,1,1,1,1],
-          [1,1,1,1,1,1,1,1],
-          [1,1,1,1,1,1,1,1],
-          [1,1,1,1,1,1,1,1]
         ]
-      }
     }
     this.toggle = this.toggle.bind(this);
   }
+
+  //alters color of each button on click
   toggle(row, col){
     var copy = this.state.board.slice();
     if (copy[row][col] === 0) {
@@ -41,15 +30,20 @@ class App extends Component {
       this.setState({board: copy});
     }
   }
+
+  //save state of board
   handleSubmit(){
-    console.log('this is the handleSubmit func');
+    $.post('/setBoard',this.state);
   }
+
   render() {
 		return (
 			<div>
 				<h1>Buddy Beats</h1>
-        <FormSave onSubmit = {this.handleSubmit}/>
-
+        <form className = "saveform" >
+          <input type="text" placeholder="name of board" />
+          <input type="submit" value="Save Board" required = {true} onSubmit = {this.handleSubmit}/>
+        </form>
 
 				<Board boxState = {this.state.board} toggle = {this.toggle}/>
         <Player board = {this.state.board} />
